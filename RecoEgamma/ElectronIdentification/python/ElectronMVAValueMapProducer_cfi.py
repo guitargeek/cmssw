587c076cddff2311945d953a7bdcbf619242f6e7
Jonas Rembser <jonas.rembser@cern.ch>
2018-08-24 14:38:03 +0200

    Added V2 TMVA training

--- RecoEgamma/ElectronIdentification/python/ElectronMVAValueMapProducer_cfi.py
+++ RecoEgamma/ElectronIdentification/python/ElectronMVAValueMapProducer_cfi.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 mvaConfigsForEleProducer = cms.VPSet( )
 
+
 # Import and add all desired MVAs
 from RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring16_HZZ_V1_cff \
     import mvaEleID_Spring16_HZZ_V1_producer_config
@@ -30,6 +31,15 @@ mvaConfigsForEleProducer.append( mvaEleID_Fall17_iso_V2_producer_config )
 from RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_iso_V2_HZZ_cff \
     import mvaEleID_Fall17_iso_V2_HZZ_producer_config
 mvaConfigsForEleProducer.append( mvaEleID_Fall17_iso_V2_HZZ_producer_config )
+
+from RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_noIso_V2_TMVA_cff \
+    import mvaEleID_Fall17_noIso_V2_TMVA_producer_config
+mvaConfigsForEleProducer.append( mvaEleID_Fall17_noIso_V2_TMVA_producer_config )
+
+from RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_iso_V2_TMVA_cff \
+    import mvaEleID_Fall17_iso_V2_TMVA_producer_config
+mvaConfigsForEleProducer.append( mvaEleID_Fall17_iso_V2_TMVA_producer_config )
+
 
 electronMVAValueMapProducer = cms.EDProducer('ElectronMVAValueMapProducer',
                                              # The module automatically detects AOD vs miniAOD, so we configure both
